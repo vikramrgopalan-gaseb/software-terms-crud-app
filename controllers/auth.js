becc9c9d0ff40a732b17a26f88ede8a84e8824b3
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-01-14 16:29:19 -0700

    1/14 v3

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,24 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user.js");
+
 const express = require("express");
 const router = express.Router();
 
 module.exports = router;
+
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+  res.send("Form submission accepted!");
+});
+
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
+
+router.post("/sign-in", async (req, res) => {
+  res.send("Request to sign in received!");
+});
--- server.js
+++ server.js
@@ -1,16 +1,87 @@
-// server.js
-
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
 
-// Connect to MongoDB using the connection string in the .env file
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+const authController = require("./controllers/auth.js");
+
+const session = require('express-session');
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000";
+
 mongoose.connect(process.env.MONGODB_URI);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+// new
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
+
+// AUTHENTICATION
+
+// server.js
+
+// GET /
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+app.use("/auth", authController);
+
+// wrap userinDatabase in async function (auth controller, fruits skyrockit sign up)
+
+const userInDatabase = await User.findOne({ username: req.body.username });
+if (userInDatabase) {
+  return res.send("Username already taken.");
+}
+
+if (req.body.password !== req.body.confirmPassword) {
+  return res.send("Password and Confirm Password must match");
+}
+
+const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+req.body.password = hashedPassword;
+
+const validPassword = bcrypt.compareSync(
+  req.body.password,
+  userInDatabase.password
+);
+if (!validPassword) {
+  return res.send("Login failed. Please try again.");
+}
+
+// validation logic
+
+const user = await User.create(req.body);
+res.send(`Thanks for signing up ${user.username}`);
+
+const userInDatabase = await User.findOne({ username: req.body.username });
+if (!userInDatabase) {
+  return res.send("Login failed. Please try again.");
+}
 
